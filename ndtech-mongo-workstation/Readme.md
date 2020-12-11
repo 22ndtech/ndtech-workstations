4cb3ee71fdf3a1f9a05ce977f674981e17846063
Jeff Hegedus <jeff@22ndtech.com>
2020-12-11 11:56:56 -0700

    Added node workstation
    and updated readme files for each workstation

--- ndtech-mongo-workstation/Readme.md
+++ ndtech-mongo-workstation/Readme.md
@@ -1,2 +1,2 @@
-# ndtech-k8s-pipeline-workstation
+# ndtech-mongo-workstation
 The *.ps1 files are convenience files for building, running and debugging the workstation image. The run.ps1 script assumes you are connected on display 0.
---
+++ ndtech-node-workstation/debug.ps1
@@ -0,0 +1,11 @@
+docker run `
+-e DISPLAY=${Env:ipv4}:0 `
+-e GITHUB_USER=$ENV:GITHUB_USER `
+-e GITHUB_TOKEN=$ENV:GITHUB_TOKEN `
+-e GITHUB_ORGANIZATION="22ndtech" `
+-e GIT_USER_NAME="$ENV:GIT_USER_NAME" `
+-e GIT_USER_EMAIL="$ENV:GIT_USER_EMAIL" `
+-v c:\data\cached-src:/work `
+-v ${PWD}/scripts:/work/scripts `
+-v ${Env:CERTS_PATH}:/openssl-certs:ro `
+--rm -it --entrypoint /bin/bash 22ndtech/ndtech-node-workstation
